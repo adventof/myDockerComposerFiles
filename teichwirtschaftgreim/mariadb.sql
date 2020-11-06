1ace402696e07ce683cd6ec5b91bb895e66b1134
adventof <45686207+adventof@users.noreply.github.com>
2020-11-06 11:00:05 +0100

    Update mariadb.sql

--- teichwirtschaftgreim/mariadb.sql
+++ teichwirtschaftgreim/mariadb.sql
@@ -9,6 +9,15 @@ GRANT ALL privileges ON `drupaldatabase0`.* TO 'drupaluser0'@localhost;
 GRANT ALL privileges ON `drupaldatabase0`.* TO 'drupaluser0'@'%';
 FLUSH PRIVILEGES;
 
+DROP DATABASE `drupaldatabase1`;
+CREATE DATABASE `drupaldatabase1`;
+CREATE USER 'drupaluser1' IDENTIFIED BY 'drupaldatabase1passwd';
+GRANT USAGE ON *.* TO 'drupaluser1'@localhost IDENTIFIED BY 'drupaldatabase1passwd';
+GRANT USAGE ON *.* TO 'drupaluser1'@'%' IDENTIFIED BY 'drupaldatabase1passwd';
+GRANT ALL privileges ON `drupaldatabase1`.* TO 'drupaluser1'@localhost;
+GRANT ALL privileges ON `drupaldatabase1`.* TO 'drupaluser1'@'%';
+FLUSH PRIVILEGES;
+
 DROP DATABASE `matomodatabase0`;
 CREATE DATABASE `matomodatabase0`;
 CREATE USER 'matomouser0' IDENTIFIED BY 'matomouser0passwd';
